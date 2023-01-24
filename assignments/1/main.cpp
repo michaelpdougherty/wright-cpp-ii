4c4519f1496b396530bec1f4c8034a95829164fb
Mike Dougherty <mikedough213@gmail.com>
2023-01-23 21:44:36 -0600

    Add *.out to .gitignore

--- assignments/1/main.cpp
+++ assignments/1/main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include <string>
 #include <map>
+using namespace std;
 
-std::map<std::string, int> TESTS = {
-  { "I", 1 },
-  { "II", 2 },
-  { "III", 3 },
-};
-
-std::map<char, int> DIGITS = {
+map<char, int> DIGITS = {
   { 'I', 1 },
   { 'V', 5 },
   { 'X', 10 },
@@ -18,16 +13,7 @@ std::map<char, int> DIGITS = {
   { 'M', 1000 },
 };
 
-void log(std::string name, std::string value) {
-  printf("%s: %s\n", name.c_str(), value.c_str());
-}
-
-void log(std::string name, int value) {
-  printf("%s: %d\n", name.c_str(), value);
-}
-
-int readRomanNumeral(std::string num)
-{
+int readRomanNumeral(string num) {
   int n = num.length();
   int sum = DIGITS[num[n - 1]];
   for (int i = n - 2; i >= 0; i--) {
@@ -43,9 +29,11 @@ int readRomanNumeral(std::string num)
 }
 
 int main() {
-  std::cout << "Looping through TESTS...\n";
-  for (auto const& [input, expected] : TESTS) {
-    printf("%s: %d\n", input.c_str(), expected);
-  }
+  string num;
+  cout << "Please enter a Roman numeral: ";
+  cin >> num;
+
+  int answer = readRomanNumeral(num);
+  printf("Answer: %d\n", answer);
   return 0;
 }
