d8aca05976998a910477b62d19b7489dd1c99d57
Mike Dougherty <mikedough213@gmail.com>
2023-02-02 21:23:16 -0600

    Updates

--- assignments/2/main.cpp
+++ assignments/2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 #include <string>
 #include <map>
@@ -26,7 +27,7 @@ map<MOVE_TYPE, int> MOVES = {
   { SMALL_SLIP, -2 },
 };
 
-map<int, int> hare_moves = {
+map<int, MOVE_TYPE> hare_moves = {
   {1, SLEEP},
   {2, SLEEP},
   {3, BIG_HOP},
@@ -39,7 +40,7 @@ map<int, int> hare_moves = {
   {10, SMALL_SLIP},
 };
 
-map<int, int> tortoise_moves = {
+map<int, MOVE_TYPE> tortoise_moves = {
   {1, FAST_PLOD},
   {2, FAST_PLOD},
   {3, FAST_PLOD},
@@ -54,25 +55,34 @@ map<int, int> tortoise_moves = {
 
 class Racer {
   public:
-    Racer(string name) {
+    Racer(string name, map<int, MOVE_TYPE> moveset, string winMessage) {
       this->name = name;
       this->initial = name[0];
+      this->moveset = moveset;
+      this->winMessage = winMessage;
     }
+    int position = 0;
     string name;
     char initial;
-    int position = 0;
+    map<int, MOVE_TYPE> moveset;
+    string winMessage;
 };
 
 class Race {
   public:
+    Race(Racer *tortoise, Racer *hare) {
+      this->racer1 = tortoise;
+      this->racer2 = hare;
+    }
     bool hasWinner = false;
     static const int TRACK_LENGTH = 70;
-    string winner;
+    Racer *winner;
     Racer *racer1;
     Racer *racer2;
     void printTrack() {
       for (int i = 0; i < Race::TRACK_LENGTH; i++) {
         cout << '[';
+        // cout << setw(4) << left;
         if (i == racer1->position && i == racer2->position) {
           cout << "OUCH!!";
         } else if (i == racer1->position) {
@@ -84,42 +94,39 @@ class Race {
       }
       cout << endl;
     }
+    void fireGun() {
+      cout << "BANG!!!!!" << endl;
+      cout << "AND THEY'RE OFF!!!!!" << endl;
+    }
+    void run() {
+      fireGun();
+      do {
+        this->printTrack();
+        racer1->position += rand() % 10 + 1;
+        racer2->position += rand() % 10 + 1;
+        if (racer1->position > Race::TRACK_LENGTH - 1) {
+          racer1->position = Race::TRACK_LENGTH - 1;
+          this->hasWinner = true;
+          this->winner = racer1;
+        }
+        if (racer2->position > Race::TRACK_LENGTH - 1) {
+          racer2->position = Race::TRACK_LENGTH - 1;
+          this->hasWinner = true;
+          this->winner = racer2;
+        }
+      } while (!this->hasWinner);
+      cout << "FLASH!!!\n";
+      this->printTrack();
+      cout << this->winner->winMessage << endl;
+    }
 };
 
-void init() {
-  srand(static_cast<unsigned int>(time(NULL)));
-  cout << "BANG!!!!!" << endl;
-  cout << "AND THEY'RE OFF!!!!!" << endl;
-}
-
 int main() {
-  init();
-  Racer hare = Racer("HARE");
-  Racer tortoise = Racer("TORTOISE");
+  srand(static_cast<unsigned int>(time(NULL)));
 
-  Race race;
-  race.racer1 = &hare;
-  race.racer2 = &tortoise;
-  do {
-    race.printTrack();
-    if (hare.position < Race::TRACK_LENGTH - 1) {
-      hare.position += rand() % 10 + 1;
-    }
-    if (hare.position > Race::TRACK_LENGTH - 1) {
-      hare.position = Race::TRACK_LENGTH - 1;
-      race.hasWinner = true;
-      race.winner = "HARE";
-    }
-    if (tortoise.position < Race::TRACK_LENGTH - 1) {
-      tortoise.position += rand() % 10 + 1;
-    }
-    if (tortoise.position > Race::TRACK_LENGTH - 1) {
-      tortoise.position = Race::TRACK_LENGTH - 1;
-      race.hasWinner = true;
-      race.winner = "TORTOISE";
-    }
-  } while (!race.hasWinner);
-  race.printTrack();
-  cout << race.winner << " WINS!!!" << endl;
-  return(0);
+  Racer hare = Racer("HARE", hare_moves, "Hare wins.");
+  Racer tortoise = Racer("TORTOISE", tortoise_moves, "TORTOISE WINS!!! YAY!!");
+
+  Race race = Race(&hare, &tortoise);
+  race.run();
 }
