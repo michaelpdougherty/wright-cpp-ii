9277b848941d40331c35a97bcb153fcb2df1ac0d
Mike Dougherty <mikedough213@gmail.com>
2023-02-23 19:59:39 -0600

    Updates

--- assignments/last/main.cpp
+++ assignments/last/main.cpp
@@ -3,6 +3,16 @@
 #include <map>
 using namespace std;
 
+// directional values
+enum NSEW { NORTH, SOUTH, EAST, WEST };
+map<NSEW, string> directions = {
+  { NORTH, "NORTH" },
+  { SOUTH, "SOUTH" },
+  { EAST, "EAST" },
+  { WEST, "WEST" },
+};
+
+// spaces on the map
 class WorldObject {
   public:
     virtual char getSymbol() = 0;
@@ -32,9 +42,22 @@ class Space : public WorldObject {
 };
 
 class Shakey : public WorldObject {
+  NSEW facingDirection = NORTH;
   public:
+    string getFacingDirection() {
+      return directions[facingDirection];
+    }
     char getSymbol() {
-      return 'S';
+      string facingDirection = getFacingDirection();
+      if (facingDirection == "NORTH") {
+          return '^';
+      } else if (facingDirection == "SOUTH") {
+        return 'v';
+      } else if (facingDirection == "EAST") {
+        return '>';
+      } else {
+        return '<';
+      }
     }
 };
 
@@ -42,6 +65,7 @@ typedef WorldObject * WorldMap[10][10];
 
 int main() {
   cout << "Starting...\n";
+  system("clear");
 
   // declare map scheme
   char mapScheme[10][10] = {
@@ -60,6 +84,8 @@ int main() {
   // declare world map
   WorldMap worldMap;
 
+  Shakey myShakey;
+
   // fill map according to map scheme
   for (int x = 0; x < 10; x++) {
     for (int y = 0; y < 10; y++) {
@@ -74,7 +100,7 @@ int main() {
           worldMap[x][y] = new Space(true);
           break;
         case 'S':
-          worldMap[x][y] = new Shakey;
+          worldMap[x][y] = &myShakey;
           break;
       }
     }
@@ -87,6 +113,15 @@ int main() {
     }
     cout << endl;
   }
+
+  cout << "\n\n";
+
+  cout << "Shakey is facing: " << myShakey.getFacingDirection() << endl;
+  cout << "Please enter a command: ";
+  string cmd;
+  cin >> cmd;
+
+  cout << "You entered: " << cmd << endl;
 
   cout << "Done.\n";
   return 0;
